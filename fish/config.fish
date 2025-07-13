5721b4ce961fa568c906bf450cea28bd7f832bf6
Clément Chausson <clement.chausson@epitech.eu>
2025-07-13 19:09:23 +0200

    Only enable custom colors for foot ternminal

--- fish/config.fish
+++ fish/config.fish
@@ -2,8 +2,10 @@ if status is-interactive
     # Starship custom prompt
     starship init fish | source
 
-    # Custom colours
-    cat ~/.local/state/caelestia/sequences.txt 2> /dev/null
+    # Only apply custom colors in foot terminal
+    if test "$TERM_PROGRAM" = "foot" -o "$TERM" = "foot" -o "$TERM" = "foot-extra"
+        cat ~/.local/state/caelestia/sequences.txt 2> /dev/null
+    end
 
     # For jumping between prompts in foot terminal
     function mark_prompt_start --on-event fish_prompt
